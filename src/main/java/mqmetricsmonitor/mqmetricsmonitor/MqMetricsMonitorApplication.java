d1e82e388923fbeef58ac7dc59017bb3b7c8000d
calvin <cschirwa@gmail.com>
2020-06-15 23:56:15 +0200

    Initial Commit

---
+++ src/main/java/mqmetricsmonitor/mqmetricsmonitor/MqMetricsMonitorApplication.java
@@ -0,0 +1,13 @@
+package mqmetricsmonitor.mqmetricsmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqMetricsMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqMetricsMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mqmetricsmonitor/mqmetricsmonitor/MqMetricsMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package mqmetricsmonitor.mqmetricsmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqMetricsMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
